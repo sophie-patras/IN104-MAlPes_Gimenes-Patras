3fc7756fe51803e36104296f42b786b4ee04afa4
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-04 23:24:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
-
+import kivy
+kivy.require('1.0.6')
+import sqlite3
 from kivymd.app import MDApp
 from carto_refuges import CartoRefuges
-import sqlite3
+from assistancegps import AssistanceGps
 
 class MainApp(MDApp):
-	connection = None
-	cursor= None
+    connection=None
+    cursor=None 
+    def on_start(self):
+        #on initialisele GPS
+        AssistanceGps().run()
+        #On se connecte a la base de donnees 
+        self.connection=sqlite3.connect("points_refuges.db")
+        self.cursor=self.connection.cursor()
 
-	def on_start(self):
-		#Initialisation du GPS
+        #initialiser la recherche 
+        pass 
 
-		# Connection la base de donnee
-		self.connection= sqlite3.connect("points_refuges.db")
-		self.cursor= self.connection.cursor()
-
-		# Paramétrage du SearchPopupMenu
-		pass
 
 MainApp().run()
