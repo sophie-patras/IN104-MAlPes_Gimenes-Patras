aee4128246700da245f164dffd17ab351a901d48
sophie-patras <62791972+sophie-patras@users.noreply.github.com>
2020-05-09 11:08:19 +0200

    lg de codes disparues --> remis à jour

--- carto_refuges.py
+++ carto_refuges.py
@@ -12,6 +12,17 @@ class CartoRefuges(MapView):
 
 	def debut_cairn_dans_champ(self):
 		""" Apres 1 sec, fait apparaitre les markets dans fov. tres utile pour que l'app fasse pas 400 000 calculs a chaque fois qu'on bouge"""
+		try:
+			self.cairn_timer.cancel()
+		except:
+			pass
+		"""TRES IMPORTANT : fonction appelée apres 1sec /param qui unfluence bcp l'efficacité de l'app"""
+		self.cairn_timer=Clock.schedule_once(self.cairn_dans_champ, 1)
+		
+	def cairn_dans_champ(self, *args):
+		"""get_bbox donne les valeurs aux bords de la carte affichée"""
+		min_lat, min_lon, max_lat, max_lon= self.get_bbox()
+		app=App.get_running_app()
 		""" requete sql"""
 		requete_sql = "SELECT * FROM points_refuges WHERE latitude > %s AND latitude< %s AND longitude< %s AND longitude> %s "%(min_lat, max_lat, max_lon, min_lon)
 		app.cursor.execute(requete_sql)
