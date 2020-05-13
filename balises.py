dc4e4ffb0da2cfc8fc66e34b807f0bad0aaffda3
sophie-patras <62791972+sophie-patras@users.noreply.github.com>
2020-05-13 19:53:27 +0200

    Ajout donnees ds le popup depuis des attributs

--- balises.py
+++ balises.py
@@ -1,27 +1,28 @@
-from kivy.lang import Builder
-from kivy.properties import ObjectProperty
+
 from kivy.garden.mapview import MapMarkerPopup
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.popup import Popup
+from menudialogx import ListMDDialog
 
 class Balises(MapMarkerPopup):
 
-	source = "icone_maison_mini.png" #dimensions adaptées
-	#point_data=[]
+	source = "icone_maison_mini.png" #dim fixes, voir dans MapMarkerPopup pour moduler
+	data=[]
 
 	def on_release(self):
-		#ouvrir menu infos
-		#menu = InfosPopup(self.point_data)
-		menu= CustomPopup(title="Caractéristiques du refuge")
-		menu.size_hint = [.6, .7]
+		menu= Caracteristiques(self.data)
+		menu.title="Caractéristiques du refuge"
+		menu.size_hint = [.5, .8]
 		menu.open()
 
-class CustomPopup(Popup):
+class Caracteristiques(ListMDDialog):
 	
-	""" Utile en fonction de la taille du popup : bouton pour fermer popup, completer aussi le .kv
-	def dismiss_popup(self):
-        	#femetue du popup
-        	self.dismiss()"""
 
-Builder.load_file("balises.kv")
+	def __init__(self, data):
+		super().__init__()
+
+		intitules=["id_point","nom","categorie","altitude","latitude","longitude","etat","nombredeplace"]
+		setattr(self,"nombredeplace", data[-1])
+		
+		for i in range(len(intitules)-2):
+            		attribute_name = intitules[i]
+            		attribute_value = data[i]
+            		setattr(self, attribute_name, attribute_value)
