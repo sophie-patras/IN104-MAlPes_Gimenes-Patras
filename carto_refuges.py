026a3a44ad6bbd0f45e74e4254e3dff02981bca9
sophie-patras <62791972+sophie-patras@users.noreply.github.com>
2020-05-13 20:44:42 +0200

    mini rectif balise.data=x

--- carto_refuges.py
+++ carto_refuges.py
@@ -1,9 +1,7 @@
 from kivy.garden.mapview import MapView
 from kivy.clock import Clock
 from kivy.app import App
-#from kivy.uix.label import Label
-#from kivy.uix.popup import Popup
-#from kivy.lang.builder import Builder
+
 from balises import Balises
 
 class CartoRefuges(MapView):
@@ -27,7 +25,7 @@ class CartoRefuges(MapView):
 		requete_sql = "SELECT * FROM points_refuges WHERE latitude > %s AND latitude< %s AND longitude< %s AND longitude> %s "%(min_lat, max_lat, max_lon, min_lon)
 		app.cursor.execute(requete_sql)
 		points_carto = app.cursor.fetchall() 	#on mettra d'autres balises que les refuges
-		print(points_carto)
+		#print(points_carto)
 		for point in points_carto:
 			nom = point[1]
 			if nom in self.noms_points:
@@ -39,7 +37,7 @@ class CartoRefuges(MapView):
 		#creer une balise
 		lat, lon = x[4],x[5]
 		balise=Balises(lat=lat, lon=lon)
-		balise.point_data=x
+		balise.data=x
 		# l'afficher
 		self.add_widget(balise)		
 		#Popup(title="Test", content=Label(text='x'),size_hint=(None,None), size=(200,200))
