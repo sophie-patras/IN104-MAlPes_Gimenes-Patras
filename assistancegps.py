64c1fd185281f067783ef17f41b7f7a7070fa2cc
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-21 17:23:09 +0200

    Update assistancegps.py

--- assistancegps.py
+++ assistancegps.py
@@ -2,7 +2,7 @@ from kivy.app import App
 
 class AssistanceGps():
     def run(self): #lance le gps 
-        #clignote
+        #on fait clignoter le marqueur gps
         clignotant_gps=App.get_running_app().root.ids.cairn.ids.clignotant
         clignotant_gps.clignote()
         #on demande permission d'activer le gps
