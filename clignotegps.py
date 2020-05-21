0124c8cce5faf6fdd71c56d0aa5fd7c66837c1f1
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-21 17:05:47 +0200

    Update clignotegps.py

--- clignotegps.py
+++ clignotegps.py
@@ -4,10 +4,10 @@ from kivy.animation import Animation
 class ClignoteGps(MapMarker):
     def clignote(self):
         #faire clignoter le point gps
-        anim=Animation(opacite_ext=0, taille_clignotant=30)
-        anim.bind(on_complete=self.reinit)
-        anim.start(self)
+        anim=Animation(opacite_ext=0, taille_clignotant=30) #on change l'opacité et la taille du cercle
+        anim.bind(on_complete=self.reinit) #quand le cercle a atteint sa taille maximale, on le réinitialise
+        anim.start(self) 
     def reinit(self,*args):
         self.opacite_ext=1
-        self.taille_clignotant=self.taille_clignotant_defaut
+        self.taille_clignotant=self.taille_clignotant_defaut #réinitialisation du cercle à sa taille de départ
         self.clignote()
