6930d34f7118c26e12620642f52fb4e3d1b0b9de
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-21 17:33:29 +0200

    Update malpes.py

--- malpes.py
+++ malpes.py
@@ -12,35 +12,35 @@ from assistancegps import AssistanceGps
 from popupmenurecherche import PopupMenuRecherche
 import sqlite3
 
-class ItemDrawer(OneLineIconListItem):
+class ItemDrawer(OneLineIconListItem): #on crée la classe des items de la barre de navigation latérale
     icon=StringProperty()
 
-class ContentNavigationDrawer(BoxLayout):
+class ContentNavigationDrawer(BoxLayout): #la classe qui contiendra tout ce qu'on veut mettre dans la barre de navigation latérale
     screen_manager=ObjectProperty()
     nav_drawer=ObjectProperty()
 
-class DrawerList(ThemableBehavior,MDList):
+class DrawerList(ThemableBehavior,MDList): #la liste des items de la barre de navigation
     def set_color_item(self,instance_item):
-        #appelée quand on appuie sur un item du menu 
-        for item in self.children:
+        #appelée quand on appuie sur un item du menu: permet de créer un effet en en changeant la couleur
+        for item in self.children: 
             if item.text_color == self.theme_cls.primary_color:
                 item.text_color = self.theme_cls.text_color
                 break 
         instance_item.text_color=self.theme_cls.primary_color
 
-class MalpesApp(MDApp):
+class MalpesApp(MDApp): # la classe de l'application
 	connection = None
 	cursor= None
 	menu_recherche = None 
 
 	def on_start(self):
 		#Initialisation du GPS
-		AssistanceGps().run()
+		AssistanceGps().run() 
 		# Connection la base de donnee
 		self.connection= sqlite3.connect("points_refuges.db")
 		self.cursor= self.connection.cursor()
 
-		# Initialiser le menu de recherche
+		# Initialisation du menu de recherche
 		self.menu_recherche= PopupMenuRecherche()
 
 
