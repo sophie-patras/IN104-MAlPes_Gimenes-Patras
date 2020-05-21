f7b1cf6a3bed40305a610a60a5718c22c7341495
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-21 16:50:28 +0200

    Update epingle.py

--- epingle.py
+++ epingle.py
@@ -1,4 +1,4 @@
 from kivy.garden.mapview import MapMarkerPopup
 
-class Epingle(MapMarkerPopup):
+class Epingle(MapMarkerPopup): #on crée une classe pour les épingles qui seront placées sur le résultats de recherche
     source="epingle.png"
