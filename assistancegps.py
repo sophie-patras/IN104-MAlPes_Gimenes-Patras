359f9baaabfa999c3819e93f3c77d7d0ad0dd6be
lucillegimenes <62749866+lucillegimenes@users.noreply.github.com>
2020-05-21 22:47:59 +0200

    Delete assistancegps.py

--- assistancegps.py
+++
@@ -1,9 +0,0 @@
-from kivy.app import App
-
-class AssistanceGps():
-    def run(self): #lance le gps 
-        #on fait clignoter le marqueur gps
-        clignotant_gps=App.get_running_app().root.ids.cairn.ids.clignotant
-        clignotant_gps.clignote()
-        #on demande permission d'activer le gps
-        pass 
